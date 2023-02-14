726448a969d4bacc83d118333740034773d83e4b
Chiemi Watanabe <chiemi@a.tsukuba-tech.ac.jp>
2023-02-14 17:01:15 +0900

    add icon description

---
+++ _next/static/MgRzy_2uu0TRUxbknoxnr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/969abcc75917032c.css","static/css/6401f2d34011dd56.css","static/chunks/pages/index-add5d6486d564eae.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()

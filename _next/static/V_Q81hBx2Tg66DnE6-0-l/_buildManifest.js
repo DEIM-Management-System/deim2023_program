98262012925c1c6c582ed4f3f8bba2e750c3041f
Chiemi Watanabe <chiemi@a.tsukuba-tech.ac.jp>
2023-02-14 21:08:15 +0900

    add presentation time

---
+++ _next/static/V_Q81hBx2Tg66DnE6-0-l/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/969abcc75917032c.css","static/css/0536fb37260389bb.css","static/chunks/pages/index-3ecc61968b81df3c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
